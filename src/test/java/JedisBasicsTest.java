6d7d0f21cd26b0d8c3f2630764a3ab6c4289775d
Thach Le <lephuocthach@gmail.com>
2023-01-04 10:55:37 +0700

    String

--- src/test/java/JedisBasicsTest.java
+++ src/test/java/JedisBasicsTest.java
@@ -1,7 +1,9 @@
+import static org.awaitility.Awaitility.await;
+
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 
@@ -15,9 +17,6 @@ class JedisBasicsTest {
     if (HostPort.getRedisPassword().length() > 0) {
       jedis.auth(HostPort.getRedisPassword());
     }
-
-    jedis.del("planets");
-    jedis.del("earth");
   }
 
   @Test
@@ -27,6 +26,19 @@ class JedisBasicsTest {
     String value = jedis.get("hello");
     Assertions.assertEquals("world", value);
 
+  }
+
+  @Test
+  void redisString() {
+    String key = "customer:001";
+    jedis.set(key, "1");
+    jedis.incr(key);
+    Assertions.assertEquals("2", jedis.get(key));
+
+    jedis.expire(key, 5);
+    Assertions.assertTrue(jedis.exists(key));
+    await().atLeast(5, TimeUnit.SECONDS);
+    Assertions.assertTrue(jedis.exists(key));
   }
 
   @AfterAll
