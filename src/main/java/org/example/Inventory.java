88df1230aa1fd4c623ff45d0c38625e9472da50f
Thach Le <lephuocthach@gmail.com>
2023-01-11 21:10:42 +0700

    Inventory: checkAvailabilityAndPurchase

--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.UUID;
 import org.example.entity.Customer;
 import org.example.entity.EventInventory;
+import org.example.entity.Purchase;
 import org.example.util.KeyHelper;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
 public class Inventory {
+
   private final Jedis jedis;
 
   public Inventory(Jedis jedis) {
@@ -25,12 +28,12 @@ public class Inventory {
   }
 
   /**
-   * Create events from the array of passed event details. Provides overrides
-   * for number of available tickets, price and ticket tier.
+   * Create events from the array of passed event details. Provides overrides for number of
+   * available tickets, price and ticket tier.
    */
   public void createEvent(EventInventory[] events, int available, int price, String tier)
       throws JsonProcessingException {
-    String keyListSku = KeyHelper.createKey("events");
+    String skuKeys = KeyHelper.createKey("events");
     for (EventInventory eventInventory : events) {
       if (available > 0) {
         eventInventory.setAvailable(available);
@@ -38,10 +41,40 @@ public class Inventory {
       if (price > 0.0) {
         eventInventory.setPrice(price);
       }
-      String keyDetail = KeyHelper.createKey("event", eventInventory.getSku());
-      jedis.sadd(keyListSku, eventInventory.getSku());
-      jedis.hmset(keyDetail, eventInventory.toMap(tier));
+      String eventKey = KeyHelper.createKey("event", eventInventory.getSku());
+      jedis.sadd(skuKeys, eventInventory.getSku());
+      jedis.hmset(eventKey, eventInventory.toMap(tier));
     }
   }
 
+  /**
+   * Check if there is sufficient inventory before making the purchase
+   */
+  public void checkAvailabilityAndPurchase(Customer customer, String sku, int quantity,
+      String tier) {
+    Pipeline pipeline = jedis.pipelined();
+    try {
+      String eventKey = KeyHelper.createKey("event", sku);
+      int available = Integer.parseInt(
+          pipeline.hget(eventKey, String.format("available:%s", tier)).get());
+      double price = Double.parseDouble(
+          pipeline.hget(eventKey, String.format("price:%s", tier)).get());
+      if (available > quantity) {
+        pipeline.hincrBy(eventKey, String.format("available:%s", tier), -quantity);
+        String orderId = UUID.randomUUID().toString();
+        Purchase purchase = new Purchase();
+        purchase.setOrderId(orderId);
+        purchase.setCustomer(customer);
+        purchase.setTier(tier);
+        purchase.setQuantity(quantity);
+        purchase.setCost(price * quantity);
+        purchase.setSku(sku);
+        purchase.setCreatedAt(System.currentTimeMillis());
+        String orderKey = KeyHelper.createKey("sales_order", orderId);
+        pipeline.hmset(orderKey, purchase.toMap());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
---
+++ src/main/java/org/example/entity/Purchase.java
@@ -0,0 +1,90 @@
+package org.example.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Purchase {
+  @JsonProperty("order_id")
+  private String orderId;
+  @JsonProperty("customer")
+  private Customer customer;
+  @JsonProperty("tier")
+  private String tier;
+  @JsonProperty("quantity")
+  private int quantity;
+  @JsonProperty("cost")
+  private double cost;
+  @JsonProperty("sku")
+  private String sku;
+  @JsonProperty("created_at")
+  private long createdAt;
+
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  public void setTier(String tier) {
+    this.tier = tier;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public void setCost(double cost) {
+    this.cost = cost;
+  }
+
+  public void setSku(String sku) {
+    this.sku = sku;
+  }
+
+  public void setCreatedAt(long createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getOrderId() {
+    return orderId;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public String getTier() {
+    return tier;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public double getCost() {
+    return cost;
+  }
+
+  public String getSku() {
+    return sku;
+  }
+
+  public long getCreatedAt() {
+    return createdAt;
+  }
+
+  public Map<String, String> toMap() {
+    Map<String, String> values = new HashMap<>();
+    values.put("order_id", this.getOrderId());
+    values.put("customer", this.getCustomer().toString());
+    values.put("tier", String.valueOf(this.getTier()));
+    values.put("qty", String.valueOf(this.getQuantity()));
+    values.put("cost", String.valueOf(this.getCost()));
+    values.put("event_sku", this.getSku());
+    values.put("created_at", String.valueOf(this.getCreatedAt()));
+    return values;
+  }
+}
