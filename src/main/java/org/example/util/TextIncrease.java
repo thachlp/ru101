a50f64736b3d01c5e2ffae2da7d1bbdff5545e1b
Thach Le <lephuocthach@gmail.com>
2023-01-17 22:38:34 +0700

    TextIncrease increaseString

--- src/main/java/org/example/util/TextIncrease.java
+++ src/main/java/org/example/util/TextIncrease.java
@@ -12,4 +12,27 @@ public class TextIncrease {
     return (char) (c + 1);
   }
 
+  /**
+   * Increment a string, including wrapping from 'Z' to 'AA' etc.
+   */
+  public static String increaseString(String s) {
+    int n = s.length();
+    int zEnd = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      if (s.charAt(i) == 'Z') {
+        zEnd++;
+      }
+    }
+    s = s.substring(0, n - zEnd);
+    int numReplacements = n - zEnd;
+    char first = s.charAt(0);
+    s = s.substring(1);
+    s = increaseChar(first) + s;
+    while (numReplacements > 0) {
+      s = s + "AA";
+      numReplacements--;
+    }
+    return s;
+  }
+
 }
